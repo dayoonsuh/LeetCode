5e6e3661f0dfed3c9c5fa016400868a5ebca7f48
dayoonsuh <89650534+dayoonsuh@users.noreply.github.com>
2022-12-27 21:32:51 -0500

    Add files via upload

---
+++ 278 First Bad Version.py
@@ -0,0 +1,20 @@
+# The isBadVersion API is already defined for you.
+# @param version, an integer
+# @return a bool
+# def isBadVersion(version):
+
+class Solution(object):
+    def firstBadVersion(self, n):
+        if n == 1:
+            return 1
+
+        left = 1
+        right = n
+        while left<=right:
+            mid = (left+right)//2
+            if left == right:
+                return right
+            elif isBadVersion(mid):
+                right = mid
+            else:
+                left = mid+1
